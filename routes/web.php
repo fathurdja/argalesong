ebefea5622539d3f73e1de4624a9fbb45ae8daaf
dhimasss <122718816+dhimasss@users.noreply.github.com>
2025-02-22 05:40:19 -0800

    updete delete interaktif

--- routes/web.php
+++ routes/web.php
@@ -85,8 +85,13 @@ Route::get('/master-data-piutang', [masterdatacontroller::class, 'index'])->name
 Route::get('/master-data-piutang-create', [masterdatacontroller::class, 'create'])->name('master_data_piutang_create');
 Route::post('/storeTipePelanggan', [masterdatacontroller::class, 'storeTipePelanggan'])->name('storeTipePelanggan');
 Route::post('/storeTipePiutang', [masterdatacontroller::class, 'storeTipePiutang'])->name('storeTipePiutang');
-// -> delete master piutang
+// // -> delete master piutang
 // Route::delete('/master-data-piutang/{id}', [masterdatacontroller::class, 'destroy'])->name('master_data_piutang_delete');
+// Route untuk menghapus item
+Route::delete('/master-data-piutang/{id}', [masterdatacontroller::class, 'destroy'])->name('master_data_piutang.destroy');
+
+
+
 
 Route::get('/kartu-pelanggan', [kp_pelangganController::class, 'index'])->name('kp_pelanggan');
 
