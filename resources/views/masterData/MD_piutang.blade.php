24be723473c8336a0cc7a09d26f3158a2d5ff36a
dhimasss <122718816+dhimasss@users.noreply.github.com>
2025-02-23 04:51:00 -0800

    update detele aktif

--- resources/views/masterData/MD_piutang.blade.php
+++ resources/views/masterData/MD_piutang.blade.php
@@ -12,21 +12,27 @@
                         <h3 class="font-semibold mb-2 text-center bg-gray-100 py-1">{{ $title }}</h3>
                         <ul class="space-y-5">
                             @foreach ($items as $item)
-                            <li class="flex items-center">
-                                <span class="mr-2">
-                                    <form action="{{ route('master_data_piutang.destroy', $item->id) }}" method="POST" onsubmit="return confirm('Apakah Anda yakin ingin menghapus {{ $item->name }}?');">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="p-2 bg-red-600 hover:bg-red-700 text-white rounded-md active:scale-95">
-                                            <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                                <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
-                                            </svg>
-                                        </button>
-                                    </form>
-                                </span>
-                                {{ $item->name }} <!-- Mengakses name dari objek -->
-                            </li>
-                        @endforeach                        
+                            @if ($item)
+                                <li class="flex items-center">
+                                    <span class="mr-2">
+                                        <form action="{{ route('master_data_destroy') }}" method="POST" onsubmit="return confirm('Apakah Anda yakin ingin menghapus {{ $item->name }}?');">
+                                            @csrf
+                                            @method('DELETE')
+                                            <input type="hidden" name="headerName" value="{{ $title }}">
+                                            <input type="hidden" name="id" value="{{ $item->id }}">
+                                            <button type="submit" class="p-2 bg-red-600 hover:bg-red-700 text-white rounded-md active:scale-95">
+                                                <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                                    <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
+                                                </svg>
+                                            </button>
+                                        </form>
+                                    </span>
+                                    {{ $item->name }} <!-- Mengakses name dari objek -->
+                                </li>
+                            @else
+                                <li class="text-red-500">Item tidak ditemukan</li>
+                            @endif
+                        @endforeach                                     
                         <li class=" ">
                             <a href="{{ route('master_data_piutang_create', ['header' => $title]) }}" class="flex flex-row justify-start items-center w-full gap-2"><div class="bg-[#0F8114] p-1 text-white hover:scale-110 active:scale-90"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" fill-rule="evenodd"><path d="m12.594 23.258l-.012.002l-.071.035l-.02.004l-.014-.004l-.071-.036q-.016-.004-.024.006l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.016-.018m.264-.113l-.014.002l-.184.093l-.01.01l-.003.011l.018.43l.005.012l.008.008l.201.092q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.003-.011l.018-.43l-.003-.012l-.01-.01z"/><path fill="currentColor" d="M9 5a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v4h4a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2h-4v4a2 2 0 0 1-2 2h-2a2 2 0 0 1-2-2v-4H5a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h4z"/></g></svg></div> <span>Tambahkan baru</span></a>
                         </li>
